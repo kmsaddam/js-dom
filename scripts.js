d78ff54aca4ee210265507c4c23ac7afb09b8a93
K M Saddam Hossain <dev.saddam@gmail.com>
2022-10-30 17:15:32 +0600

    Query Selector

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// let val;
+// val = this;
+// console.log(val);
+
+
+// let links = document.links;
+// console.log(links);
+
+
+// // Array.from(links).forEach(link => {
+// Array.from(links).forEach(link => {
+//     //console.log(link.getAttribute('href'));
+// });
+
+
+// let  linkSingle = document.links[0].href;
+
+// // let newLinks = document.links;
+
+// // let queryLinks = document.querySelector(newLinks)
+// // queryLinks.forEach(item => {
+// //     console.log(item);
+// // });
+
+// console.log(linkSingle);
+
+
+let list = document.querySelectorAll('ol li');
+//console.log(list);
+list.forEach(item => {
+    console.log(item);
+});
